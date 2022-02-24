21bc306a1633be2741f6d03ad3f616b04acc853b
Chailar <1424570165@qq.com>
2022-02-24 20:13:26 +0800

    测试自动化

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+GITHUB_REPO=git@github.com:Chailar/Study-Notes.git
+
+comment=$1
+
+if [ ! $comment ]; then
+comment="更新内容+action自动部署"
+fi
+
+git add -A
+
+git commit -m "${comment}"    # $1 是启动该脚本传来的参数，如 sh push.sh hello，其中 hello 就会插入到 $1 处，如果想两个参数，则加 $2
+
+git push $GITHUB_REPO   
