35673d75b29949d5fd4430cf7dcb7133dd7347e9
Chailar <1424570165@qq.com>
2022-02-24 16:21:04 +0800

    “初始化项目”

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  title: 'Chailar\'s blog',
+  description: '个人笔记博客',
+  head: [
+    ['link', { rel: 'icon', href: 'img/logo.png' }],
+    ['link', {
+      rel: 'stylesheet', href: '//at.alicdn.com/t/font_3114978_qe0b39no76.css'
+    }]
+  ],
+  theme: 'vdoing',
+  themeConfig: {
+    logo: 'img/logo.png',
+    category: false,
+    tag: false,
+    archive: false,
+    updateBar: {
+      showToArticle: false
+    },
+    sidebar: 'structuring',
+    nav: [{
+      text: '前端', link: 'note/web/', items: [
+        {
+          text: '三剑客', link: '/note/web/', items: [
+            { text: 'HTML', link: '/note/web/sjk/html/' },
+            {
+              text: 'CSS', link: '/note/web/sjk/css/'
+            }, {
+              text: 'JavaScript', link: '/note/web/sjk/js/'
+            }
+          ]
+        }, {
+          text: '表单', link: '/pages/f4b2f7/'
+        }, {
+          text: '框架', items: [
+            { text: 'JQuery', link: '/pages/baf642/' }
+          ]
+        }
+      ]
+    }, {
+      text: 'Java', items: [{
+        text: 'Java容器', items: [{
+          text: 'JavaWeb', link: '/note/java/javaEE/'
+        }]
+      }]
+    }, {
+      text: '工具', link: '/note/tools/', items: [{
+        text: 'markdown', link: '/pages/c314ae/'
+      }, {
+        text: 'git', link: '/note/tools/git/'
+      }]
+    }, {
+      text: ' 学校科目', link: '/note/school/'
+    }
+    ]
+  },
+  markdown: {
+    lineNumbers: false
+  },
+  plugins: [['one-click-copy', {
+    copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'],
+    copyMessage: '白嫖成功!',
+    toolTipMessage: '立即白嫖',
+    duration: 300,
+  }], ['zooming', {
+    selector: '.theme-vdoing-content img:not(.no-zoom)',
+    options: {
+      bgColor: 'rgba(0,0,0,0.6)',
+    },
+  }],[{
+    name: 'custom-plugins',
+    globalUIComponents: ["GlobalTip"] // 2.x 版本 globalUIComponents 改名为 clientAppRootComponentFiles
+  }]]
+}
---
+++ docs/index.md
@@ -0,0 +1,20 @@
+---
+home: true
+heroText: Chailar's blog
+tagline: 永不放弃，永不放弃又有两个原则，第一个原则是永不放弃，第二个原则就是：当你想放弃时回头看第一个原则。
+bannerBg: none
+postList: none
+
+features:
+  - title: 前端
+    imgUrl: /img/jmx.png
+    link: /note/web
+  - title: 后端
+    imgUrl: /img/mycf.png
+    link: /note/java/javaEE
+  - title: 技术
+    imgUrl: /img/other.png
+
+footer: MIT Licensed | Copyright © 2021-present Chailar You
+---
+
